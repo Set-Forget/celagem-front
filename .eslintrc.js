bedf8ad134e8abc908df59016d7ff72c26b23ee7
Agustin Delgado <agustindelgado@setandforget.io>
2025-03-19 11:07:46 -0300

    hotfix

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
 		"@typescript-eslint/naming-convention": "off",
 		"@typescript-eslint/no-explicit-any": "off",
 		"@typescript-eslint/no-unused-vars": "off",
+		'@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
 	}
 };
