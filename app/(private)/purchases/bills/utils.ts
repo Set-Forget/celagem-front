fe328471c652f76da336e3f7263191dec4f79e5a
Agustin Delgado <agustindelgado@setandforget.io>
2025-03-25 09:33:52 -0300

    hotfix

--- app/(private)/purchases/bills/utils.ts
+++ app/(private)/purchases/bills/utils.ts
@@ -4,6 +4,7 @@ export const billStatus = {
     label: "Borrador",
     bg_color: "bg-gray-100",
     text_color: "text-gray-800",
+    pure_bg_color: "bg-gray-500",
   },
   overdue: {
     label: "Vencida",
@@ -15,16 +16,19 @@ export const billStatus = {
     label: "A aprobar",
     bg_color: "bg-amber-100",
     text_color: "text-amber-800",
+    pure_bg_color: "bg-amber-500",
   },
   posted: {
     label: "A recibir",
     bg_color: "bg-blue-100",
     text_color: "text-blue-800",
+    pure_bg_color: "bg-blue-500",
   },
   cancel: {
     label: "Cancelada",
     bg_color: "bg-red-100",
     text_color: "text-red-800",
+    pure_bg_color: "bg-red-500",
   },
   done: {
     label: "Paga",
