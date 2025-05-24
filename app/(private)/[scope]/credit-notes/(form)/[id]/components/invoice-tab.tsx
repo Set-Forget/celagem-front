54e18d63e43dbfaab316c8ae785537409e5bbf54
Agustin Delgado <agustindelgado@setandforget.io>
2025-05-24 12:28:20 -0300

    fix(invoice-tab): update link routing in fields to handle purchases and sales scopes

--- app/(private)/[scope]/credit-notes/(form)/[id]/components/invoice-tab.tsx
+++ app/(private)/[scope]/credit-notes/(form)/[id]/components/invoice-tab.tsx
@@ -10,44 +10,53 @@ import { es } from "date-fns/locale";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
-const fields: FieldDefinition<InvoiceDetail>[] = [
-  {
-    label: "Número",
-    placeholderLength: 13,
-    render: (p) => <Button
-      variant="link"
-      className="p-0 h-auto text-foreground font-normal"
-      asChild
-    >
-      <Link href={routes.invoice.detail(p.id)} target="_blank">
-        {p.number}
-      </Link>
-    </Button>,
-  },
-  {
-    label: "Fecha de emisión",
-    placeholderLength: 13,
-    render: (p) => p.date ? format(parseISO(p.date), "dd MMM yyyy", { locale: es }) : "No especificado",
-  },
-  {
-    label: "Fecha de vencimiento",
-    placeholderLength: 13,
-    render: (p) => p.due_date ? format(parseISO(p.due_date), "dd MMM yyyy", { locale: es }) : "No especificado",
-  },
-  {
-    label: "Condición de pago",
-    placeholderLength: 10,
-    render: (p) => p.payment_term.name || "No especificado",
-  },
-  {
-    label: "Método de pago",
-    placeholderLength: 10,
-    render: (p) => p.payment_method?.name || "No especificado",
-  }
-]
-
 export default function InvoiceTab() {
-  const { id } = useParams<{ id: string }>()
+  const { id, scope } = useParams<{ id: string, scope: "purchases" | "sales" }>()
+
+  const fields: FieldDefinition<InvoiceDetail>[] = [
+    {
+      label: "Número",
+      placeholderLength: 13,
+      render: (p) => <Button
+        variant="link"
+        className="p-0 h-auto text-foreground font-normal"
+        asChild
+      >
+        <Link
+          href={
+            p?.id
+              ? (scope === "purchases"
+                ? String(routes.bill.detail(p.id))
+                : String(routes.invoice.detail(p.id)))
+              : ""
+          }
+          target="_blank"
+        >
+          {p?.number || "No especificado"}
+        </Link>
+      </Button>,
+    },
+    {
+      label: "Fecha de emisión",
+      placeholderLength: 13,
+      render: (p) => p.date ? format(parseISO(p.date), "dd MMM yyyy", { locale: es }) : "No especificado",
+    },
+    {
+      label: "Fecha de vencimiento",
+      placeholderLength: 13,
+      render: (p) => p.due_date ? format(parseISO(p.due_date), "dd MMM yyyy", { locale: es }) : "No especificado",
+    },
+    {
+      label: "Condición de pago",
+      placeholderLength: 10,
+      render: (p) => p.payment_term.name || "No especificado",
+    },
+    {
+      label: "Método de pago",
+      placeholderLength: 10,
+      render: (p) => p.payment_method?.name || "No especificado",
+    }
+  ]
 
   const { data: creditNote, isLoading: isCreditNoteLoading } = useGetCreditNoteQuery(id)
   const { data: invoice, isLoading: isInvoiceLoading } = useGetInvoiceQuery(String(creditNote?.associated_invoice?.id ?? ""), {
